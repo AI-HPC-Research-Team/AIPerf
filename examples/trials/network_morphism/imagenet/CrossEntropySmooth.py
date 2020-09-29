0a37b89b018b885d4cbfa14e4e777eaf51951750
wanghua <wanghua36@huawei.com>
2020-09-29 16:05:51 +0800

    adapte to mindspore 1.0

--- examples/trials/network_morphism/imagenet/CrossEntropySmooth.py
+++ examples/trials/network_morphism/imagenet/CrossEntropySmooth.py
@@ -1,5 +1,4 @@
 # Copyright 2020 Huawei Technologies Co., Ltd
-# Copyright (c) Peng Cheng Laboratory 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,28 +13,26 @@
 # limitations under the License.
 # ============================================================================
 """define loss function for network"""
-from mindspore.nn.loss.loss import _Loss
-from mindspore.ops import operations as P
-from mindspore.ops import functional as F
+import mindspore.nn as nn
 from mindspore import Tensor
 from mindspore.common import dtype as mstype
-import mindspore.nn as nn
+from mindspore.nn.loss.loss import _Loss
+from mindspore.ops import functional as F
+from mindspore.ops import operations as P
 
 
-class CrossEntropy(_Loss):
-    """the redefined loss function with SoftmaxCrossEntropyWithLogits"""
-
-    def __init__(self, smooth_factor=0., num_classes=1001):
-        super(CrossEntropy, self).__init__()
+class CrossEntropySmooth(_Loss):
+    """CrossEntropy"""
+    def __init__(self, sparse=True, reduction='mean', smooth_factor=0., num_classes=1000):
+        super(CrossEntropySmooth, self).__init__()
         self.onehot = P.OneHot()
+        self.sparse = sparse
         self.on_value = Tensor(1.0 - smooth_factor, mstype.float32)
         self.off_value = Tensor(1.0 * smooth_factor / (num_classes - 1), mstype.float32)
-        self.ce = nn.SoftmaxCrossEntropyWithLogits()
-        self.mean = P.ReduceMean(False)
-
+        self.ce = nn.SoftmaxCrossEntropyWithLogits(reduction=reduction)
 
     def construct(self, logit, label):
-        one_hot_label = self.onehot(label, F.shape(logit)[1], self.on_value, self.off_value)
-        loss = self.ce(logit, one_hot_label)
-        loss = self.mean(loss, 0)
+        if self.sparse:
+            label = self.onehot(label, F.shape(logit)[1], self.on_value, self.off_value)
+        loss = self.ce(logit, label)
         return loss
