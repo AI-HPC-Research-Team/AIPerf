2cb867880ddbb1e380e53d7fdc53ff88463afd6c
AI-HPC-Research-Team <renzhx@pcl.ac.cn>
2020-10-17 17:07:35 +0800

    Update data transmission script.

--- scripts/deploy/data_transmission/data_transmission.sh
+++ scripts/deploy/data_transmission/data_transmission.sh
@@ -2,9 +2,7 @@
 
 #获取本机ip
 read -p "请输入本机ip:" LOCAL_IP
-
-# 数据集路径
-IMAGENET_tar='imagenet_tar'
+read -p "数据集路径:" IMAGENET_tar
 
 # 所有节点ssh信息
 username='root'
@@ -14,21 +12,19 @@ timeout=10
 
 # 为每个机架第一台机器传输数据集
 count=1
-cat ip_data_transmission.txt | while read info;
+cat data_transmission_ip.txt | while read info;
 do
 	IP=$(echo $info|awk '{print $1}')
 	echo $IP
 	echo $count
-	FRAME=$(echo $info|awk '{print $2}')
 	# 传数据集
-	sshpass -p "$password" scp -P $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout -r $IMAGENET_tar $username@$IP:~/
+	sshpass -p "$password" rsync -r -e "ssh -p $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout" --progress $IMAGENET_tar $username@$IP:~/
 	# 传子脚本
-	sshpass -p "$password" scp -P $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout sub_data_transmission.sh $username@$IP:~/
+	sshpass -p "$password" rsync -r -e "ssh -p $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout" --progress sub_data_transmission.sh $username@$IP:~/
 	# 后台执行子脚本，机架内做数据集传输
-	sshpass -p "$password" ssh -n -p $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout $username@$IP "bash ~/sub_data_transmission.sh $IP $FRAME >/dev/null 2>&1 &"
+	sshpass -p "$password" ssh -n -p $port -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout $username@$IP "bash ~/sub_data_transmission.sh $IP $IMAGENET_tar >/dev/null 2>&1 &"
 	count=$((count+1))
 done	
 
 # 本机做机架内数据集传输
-FRAME='第一机架'
-bash sub_data_transmission.sh $LOCAL_IP $FRAME
+bash sub_data_transmission.sh $LOCAL_IP $IMAGENET_tar
